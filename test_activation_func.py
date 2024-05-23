6199c146b75f823e090d48c238142db2b574bb53
MaximeRenaud2003 <maxime.renaud@edu.ece.fr>
2024-05-23 13:48:05 +0000

    test filters, activation funcs, reduction de neurones

--- test_activation_func.py
+++ test_activation_func.py
@@ -17,15 +17,14 @@ IS_BIASED = True
 # Enable 32-bit floating-point precision
 tf.keras.backend.set_floatx('float32')
 
-for i in range (0, filters.nb_filters ) :
-    model = training_model.create_model(1,[i],1,[30],1,0.005)
+for i in range (0, activation_functions.nb_activation_func ) :
+    model = training_model.create_model(1,[3],1,[30],i,0.005)
 
     # Résumé du modèle
     #model.summary()
 
-    log_dir = "./logs/fit/"+str(i)+"filter" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = "./logs/fit/"+str(i)+"func" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    model.fit(training.training_inputs,training.training_outputs,epochs=20,validation_data=(test.test_inputs,test.test_outputs),callbacks=[tensorboard_callback])
+    model.fit(training.training_inputs,training.training_outputs,epochs=30,validation_data=(test.test_inputs,test.test_outputs),callbacks=[tensorboard_callback])
     
-
--- test_reduc_neuronnes.py
+++ test_reduc_neuronnes.py
@@ -11,21 +11,18 @@ import activation_functions
 import test
 import training
 
-IS_BIASED = True
-
 
 # Enable 32-bit floating-point precision
 tf.keras.backend.set_floatx('float32')
 
-for i in range (0, filters.nb_filters ) :
-    model = training_model.create_model(1,[i],1,[30],1,0.005)
+for i in range (1,5) :
+    model = training_model.create_model(1,[3],1,[i],2,0.005)
 
     # Résumé du modèle
     #model.summary()
 
-    log_dir = "./logs/fit/"+str(i)+"filter" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = "./logs/fit/"+str(i)+"N" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    model.fit(training.training_inputs,training.training_outputs,epochs=20,validation_data=(test.test_inputs,test.test_outputs),callbacks=[tensorboard_callback])
+    model.fit(training.training_inputs,training.training_outputs,epochs=30,validation_data=(test.test_inputs,test.test_outputs),callbacks=[tensorboard_callback])
     
-
