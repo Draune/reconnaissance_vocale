4d97e8bf268de921795279d9f35a0fc932954652
Draune <sioul.duaner@gmail.com>
2024-05-24 11:52:15 +0100

    test complet sans NN

---
+++ arduino/src/CNNdata.h
@@ -0,0 +1,7 @@
+#ifndef CNNDATA_H
+#define CNNDATA_H
+
+#define CNNDATA_INPUT_SIZE_X 48
+#define CNNDATA_INPUT_SIZE_Y 13
+
+#endif
--- arduino/src/maxpool.h
+++ arduino/src/maxpool.h
@@ -5,8 +5,8 @@
 
 #define POOL_INPUT_SIZE_X CONV_OUTPUT_SIZE_X
 #define POOL_INPUT_SIZE_Y CONV_OUTPUT_SIZE_Y
-#define POOL_OUTPUT_SIZE_X (POOL_INPUT_SIZE_X)/2
-#define POOL_OUTPUT_SIZE_Y (POOL_INPUT_SIZE_Y)/2
+#define POOL_OUTPUT_SIZE_X ((POOL_INPUT_SIZE_X)/2)
+#define POOL_OUTPUT_SIZE_Y ((POOL_INPUT_SIZE_Y)/2)
 // taille du maxpool pour chacune des 2 dimensions
 #define POOL_X 2
 #define POOL_Y 2
